1f298f44b6e35b1bbc9480d87b84889399e2ce66
hammerinformation <51826786+hammerinformation@users.noreply.github.com>
2020-07-08 16:10:49 +0300

    .
    .

---
+++ Health Bar/Assets/Script/HealtBar.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class HealtBar : MonoBehaviour
+{
+
+    /*
+     
+        https://github.com/hammerinformation
+
+     */
+    [SerializeField] private Image healthImage;
+    const float maxHealt = 100f;
+    [SerializeField] private float health = 100f;
+
+    
+
+
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+        healthImage.fillAmount = health / maxHealt;
+
+    }
+
+    private void LateUpdate()
+    {
+
+        //Example
+        if (Input.GetMouseButtonDown(1))
+        {
+            Damage(10);
+        }
+    }
+
+
+
+}
